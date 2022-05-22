0f3461a7c4e119c793684dd23a32b22e9014a36d
superjb1664 <jbaubry25@gmail.Com>
2022-05-22 23:36:58 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php
+
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=gsb;charset=utf8', 'root', '');
+
+
+
+echo "<H1>Logiciel E5 - RH !!!</H1>";
+
+if (function_exists('curl_init'))
+{
+    $curl = curl_init();
+
+    curl_setopt($curl, CURLOPT_URL, "jbaubry.fr/E5.php");
+    curl_setopt($curl, CURLOPT_PORT, "443");
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $output = curl_exec($curl);
+    echo "<H2>Réponse jbaubry.fr : $output</H2>";
+}
+else
+{
+    echo "apt install php-curl puis redémarrer systeme";
+}
+
+if(isset($_REQUEST["etat"])) {
+    $etat = (int) $_REQUEST["etat"];
+}
+else
+    $etat = 0;
+switch($etat)
+{
+    case    0:
+        AfficherListe($bdd);
+        AfficherAjouterLigne();
+        break;
+    case 1:
+        AfficherFormulaireAjout();
+        break;
+    case 2:
+        if(isset($_REQUEST["valeur"]))
+        {
+            if($_REQUEST["valeur"] != "")
+            {
+
+                AjouterValeur($bdd, $_REQUEST["valeur"]);
+                echo " $_REQUEST[valeur] ajouté !";
+            }
+        }
+        AfficherListe($bdd);
+        AfficherAjouterLigne();
+        break;
+}
+
+function AjouterValeur($bdd, $valeur)
+{
+    $req = $bdd->prepare('INSERT INTO table_www( value) VALUES( :value)');
+    $req->execute(array(
+        'value' => $valeur
+    ));
+}
+
+function AfficherFormulaireAjout()
+{
+    echo "
+<div>
+    <form> 
+        Valeur : <input type='text' name='valeur'><br>
+        <input type='hidden' name='etat' value='2'>
+        <input type='submit' value ='Ajouter'>
+    </form>
+</div>";
+}
+
+function AfficherAjouterLigne(){
+    echo "
+<div>
+    <form> 
+        <input type='hidden' name='etat' value='1'>
+        <input type='submit' value ='ajouter'>
+    </form>
+</div>";
+}
+function AfficherListe($bdd)
+{
+
+    $reponse = $bdd->query('SELECT * FROM table_www');
+    $table = $reponse->fetchAll();
+// On affiche chaque entrée une à une
+    echo "<table>
+            <tr>
+                <th> id </th><th>valeur</th>
+            </tr> 
+"           ;
+    if(count($table) > 0) {
+        foreach ($table as $ligne) {
+            echo "
+                    <tr>
+                        <td> $ligne[id] </td><td> $ligne[value]</td>
+                    </tr> 
+        ";
+        }
+    }
+    else
+    {
+        echo "<tr><td colspan='2'>Pas encore d'enregistrement</td></tr>";
+    }
+    echo "</table>";
+}
